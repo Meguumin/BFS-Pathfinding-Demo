2487b35962164fe92d35084276f518b6773f0253
Aiden Tong <1140987@edu.burnabyschools.ca>
2023-08-14 19:50:24 -0700

    Web Build, Custom Rows/Col and bug fixes

--- PathFinding/PathFinding.cpp
+++ PathFinding/PathFinding.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include "raylib.h"
 #include <vector>
 #include <deque>
 #include <time.h>
 #include <chrono>
-#include "raylib.h"
 
 struct SuperRectangle
 {
@@ -50,7 +50,7 @@ void ChangeCellState(int t, Vector2 mousePoint, std::vector<std::vector<SuperRec
                     startplace = false;
                     QueueofCells.pop_front();
                 }
-                if (Cells[i][j].state == 4)
+                if (Cells[i][j].state == 3)
                 {
                     endplace = false;
                 }
@@ -64,39 +64,67 @@ void ChangeCellState(int t, Vector2 mousePoint, std::vector<std::vector<SuperRec
     }
 }
 
+void reset(bool& found, bool& startplace, bool& endplace, bool& timerstarted, std::vector<std::vector<SuperRectangle>>& Cells, std::deque<SuperRectangle>& QueueofCells)
+{
+    found = false;
+    startplace = false;
+    endplace = false;
+    timerstarted = false;
+    for (int i = 0; i < Cells.size(); ++i) {
+        for (int j = 0; j < Cells[i].size(); ++j) {
+            Cells[i][j].state = 0;
+            Cells[i][j].visited = false;
+        }
+    }
+    QueueofCells.clear();
+}
+// Add UI so you can dynamically change the map col and row realtime 
+// make it so after 30 col and row it changes cell size and gap based on monitor size
+// add extra header to remove functions
+// Add emscripten auto build
+// make ui sidebar so you can see path, time and stuff
+// maze generation 
+// 
+
+
 int main(void)
 {
-    const int screenWidth = 1020;
-    const int screenHeight = 1020;
-    int mapcol = 20, maprow = 20;
-    int gap = 1;
-    int timer = 0;
+    int mapcol = 0, maprow = 0;
+    std::cout << "Enter number of rows: ";
+    std::cin >> maprow;
+    std::cout << "Enter number of columns: ";
+    std::cin >> mapcol;
+    int cellsize = 50;
+    float gap = 1;
     bool found = false;
     bool startplace = false;
     bool endplace = false;
     bool timerstarted = false;
+    const int screenWidth = mapcol * 50 + gap * mapcol;
+    const int screenHeight = maprow  * 50 + gap * maprow;
+   // if(screenWidth )
     std::deque<SuperRectangle> QueueofCells;
-    std::vector<std::vector<SuperRectangle>> Cells; // 2D vector of rectangles
-    SuperRectangle newCell;
+    std::vector<std::vector<SuperRectangle>> Cells; 
+    SuperRectangle newcell;
     Vector2 mousePoint = GetMousePosition();
     std::chrono::time_point<std::chrono::steady_clock> begin, end;
     float elapsed_secs = 0.0f;
-
-    for (int i = 0; i < mapcol; ++i) {
+    for (int i = 0; i < maprow; ++i) 
+    {
         std::vector<SuperRectangle> rowCells;
-        for (int j = 0; j < maprow; ++j) {
-            int x = i * (50 + gap);
-            int y = j * (50 + gap);
-            Setupcell(newCell, x, y, i, j);
-            rowCells.push_back(newCell);
+        for (int j = 0; j < mapcol; ++j) 
+        { 
+            int x = j * (cellsize + float(gap)); 
+            int y = i * (cellsize + float(gap));
+            Setupcell(newcell, x, y, i, j);
+            rowCells.push_back(newcell);
         }
         Cells.push_back(rowCells);
     }
-
     InitWindow(screenWidth, screenHeight, "Pathfinding");
-    SetTargetFPS(60);
+    SetTargetFPS(0);
 
-    while (!WindowShouldClose())
+    while (WindowShouldClose() == false)
     {
         if (timerstarted) {
             end = std::chrono::steady_clock::now();
@@ -110,7 +138,10 @@ int main(void)
         {
             ChangeCellState(1, mousePoint, Cells, QueueofCells, startplace, endplace);
         }
-
+        if (IsKeyPressed(KEY_T))
+        {
+            reset(found, startplace, endplace, timerstarted, Cells, QueueofCells);
+        }
         if (IsKeyDown(KEY_E) && !startplace)
         {
             ChangeCellState(2, mousePoint, Cells, QueueofCells, startplace, endplace);
@@ -134,7 +165,8 @@ int main(void)
             timerstarted = true;
         }
 
-        if (elapsed_secs > 0.02 && !found) {
+       if (elapsed_secs > 0.02 && !found && timerstarted)
+        {
             SuperRectangle currentCell = QueueofCells.front();
             int x = currentCell.x;
             int y = currentCell.y;
@@ -143,10 +175,11 @@ int main(void)
             if (!QueueofCells.empty()) {
                 QueueofCells.pop_front();
             }
+          
 
             if (currentCell.state != 3) {
                 // Check and push adjacent cells if they are not walls (state != 1) and not visited yet.
-                if (x + 1 < 20 && !Cells[x + 1][y].visited && Cells[x + 1][y].state != 1) {
+                if (x + 1 < maprow && !Cells[x + 1][y].visited && Cells[x + 1][y].state != 1) {
                     QueueofCells.push_back(Cells[x + 1][y]);
                     Cells[x + 1][y].parentx = x;
                     Cells[x + 1][y].parenty = y;
@@ -160,7 +193,7 @@ int main(void)
                     Cells[x - 1][y].visited = true;
                 }
 
-                if (y + 1 < 20 && !Cells[x][y + 1].visited && Cells[x][y + 1].state != 1) {
+                if (y + 1 < mapcol && !Cells[x][y + 1].visited && Cells[x][y + 1].state != 1) {
                     QueueofCells.push_back(Cells[x][y + 1]);
                     Cells[x][y + 1].parentx = x;
                     Cells[x][y + 1].parenty = y;
